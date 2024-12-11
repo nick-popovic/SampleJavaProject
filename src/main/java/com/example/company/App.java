651a1e096668a5d19a810f5c03248c372dfc3fa8
Nick Popovic <nickop.development@gmail.com>
2024-12-11 18:25:44 -0500

    Fetch and print the title of a webpage using Jsoup

--- src/main/java/com/example/company/App.java
+++ src/main/java/com/example/company/App.java
@@ -1,13 +1,24 @@
 package com.example.company;
 
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+
+        Document doc = Jsoup.connect("http://example.com/").get();
+        System.out.println(doc.title());
+
+        
     }
 }
