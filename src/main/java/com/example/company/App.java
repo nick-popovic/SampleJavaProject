06c8577f98deeb26770cfa4da76d1f02f83c7fed
Nick Popovic <nickpop.development@gmail.com>
2024-12-15 22:14:39 -0500

    Refactor POM and App class: update project name, remove unused dependencies, and change assembly plugin

--- src/main/java/com/example/company/App.java
+++ src/main/java/com/example/company/App.java
@@ -1,23 +1,13 @@
 package com.example.company;
 
-import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException
+    public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-
-        Document doc = Jsoup.connect("https://google.ca/").get();
-        System.out.println(doc.title());
-
-        
+        System.out.println( "Hello World! " );
     }
 }
